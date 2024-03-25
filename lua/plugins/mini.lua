79b2fbd72daaa0439d92570d593d6f6b5825ef78
Rahul Garg <rg@vihu.dev>
2024-03-24 22:23:20 -0600

    update: rm mini.animate, add neoscroll

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -26,37 +26,6 @@ return {
     }
     vim.keymap.set('n', '<leader>tw', ':lua MiniTrailspace.trim()<CR>', { desc = '[T]railing [W]hitespace' })
 
-    -- Nicer animations when moving around
-    require('mini.animate').setup {
-      {
-        cursor = {
-          timing = function(_, n)
-            return 150 / n
-          end,
-        },
-        scroll = {
-          timing = function(_, n)
-            return 150 / n
-          end,
-        },
-        resize = {
-          timing = function(_, n)
-            return 150 / n
-          end,
-        },
-        open = {
-          timing = function(_, n)
-            return 5 / n
-          end,
-        },
-        close = {
-          timing = function(_, n)
-            return 5 / n
-          end,
-        },
-      },
-    }
-
     -- Comment support
     require('mini.comment').setup {
       mappings = {
