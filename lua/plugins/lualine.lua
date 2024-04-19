0103112ee0754ac14433f45e8a483f0aec925f03
Rahul Garg <rg@vihu.dev>
2024-04-18 21:42:33 -0600

    rm: arrow plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,8 +4,6 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    local arrow_statusline = require 'arrow.statusline'
-
     require('lualine').setup {
       options = {
         icons_enabled = true,
@@ -24,15 +22,7 @@ return {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { { 'filename', path = 1 } },
-        lualine_x = {
-          'filetype',
-          {
-            function()
-              local statusline = require 'arrow.statusline'
-              return statusline.is_on_arrow_file() and statusline.text_for_statusline_with_icons() or ''
-            end,
-          },
-        },
+        lualine_x = { 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location', 'searchcount' },
       },
