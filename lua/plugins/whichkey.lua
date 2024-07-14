fa5e928f72e805a9e0a3f81eec40ee23879110c7
Rahul Garg <rg@vihu.dev>
2024-07-14 04:28:18 -0400

    fixup: whichkey

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,41 +1,14 @@
 return {
   'folke/which-key.nvim',
-  event = 'VimEnter',
+  event = 'VeryLazy',
   config = function()
-    require('which-key').setup {
-      window = {
-        winblend = 10,
-      },
-      triggers_blacklist = {
-        -- Disable which-key in normal mode for these keys
-        n = { "'", 'v', 'z', 'c' },
-      },
-    }
-
-    -- Document existing key chains
-    require('which-key').add {
-      { ',b', group = '[B]uffer' },
-      { ',b_', hidden = true },
-      { '<leader>b', group = '[B]uffer' },
-      { '<leader>b_', hidden = true },
-      { '<leader>c', group = '[C]ode' },
-      { '<leader>c_', hidden = true },
+    local wk = require 'which-key'
+    wk.add {
       { '<leader>f', group = '[F]ind' },
-      { '<leader>f_', hidden = true },
-      { '<leader>g', group = '[G]it' },
-      { '<leader>g_', hidden = true },
-      { '<leader>m', group = '[M]arkdown' },
-      { '<leader>m_', hidden = true },
+      { '<leader>x', group = '[T]rouble' },
       { '<leader>o', group = '[O]bsidian' },
-      { '<leader>o_', hidden = true },
-      { '<leader>r', group = '[R]ename' },
-      { '<leader>r_', hidden = true },
-      { '<leader>t', group = '[T]rim' },
-      { '<leader>t_', hidden = true },
-      { '<leader>w', group = '[W]orkspace' },
-      { '<leader>w_', hidden = true },
-      { '<leader>x', group = '[X]trouble' },
-      { '<leader>x_', hidden = true },
+      { '<leader>g', group = '[G]it' },
+      { '<leader>b', group = '[B]uffer' },
     }
   end,
 }
