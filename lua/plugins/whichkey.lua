a90acfe3c7690aacebb3c6fceba190df4d77c4f4
Rahul Garg <rg@vihu.dev>
2024-07-14 15:28:42 +0530

    update: which-key config

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,14 +1,14 @@
 return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
-  config = function()
-    local wk = require 'which-key'
-    wk.add {
+  opts = {
+    preset = 'helix',
+    spec = {
       { '<leader>f', group = '[F]ind' },
       { '<leader>x', group = '[T]rouble' },
       { '<leader>o', group = '[O]bsidian' },
       { '<leader>g', group = '[G]it' },
       { '<leader>b', group = '[B]uffer' },
-    }
-  end,
+    },
+  },
 }
