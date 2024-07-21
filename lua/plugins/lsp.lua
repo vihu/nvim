8acbe2d2a5e460ba2b7e9412f2286fe7972fe2a3
Rahul Garg <rg@vihu.dev>
2024-07-21 16:06:32 +0530

    fix: ignore line break for pylsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -110,7 +110,7 @@ return {
           pylsp = {
             plugins = {
               pycodestyle = {
-                ignore = { 'W391' },
+                ignore = { 'W391', 'W503' },
                 maxLineLength = 120,
               },
             },
