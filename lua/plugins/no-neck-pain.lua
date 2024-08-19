368512ef2437a391794e20e99f6f1407a354331b
Rahul Garg <rg@vihu.dev>
2024-08-19 05:46:22 +0530

    rm: nvim-tree, add: neo-tree, no-neck-pain

---
+++ lua/plugins/no-neck-pain.lua
@@ -0,0 +1,12 @@
+return {
+  'shortcuts/no-neck-pain.nvim',
+  version = '*',
+  opts = {
+    width = 140,
+    fallbackOnBufferDelete = true,
+    autocmds = {
+      enableOnVimEnter = true,
+      reloadOnColorSchemeChange = true,
+    },
+  },
+}
