1053c94ffadd8eb036778f7da36087011ba02324
Rahul Garg <rg@vihu.dev>
2024-12-23 19:51:09 +0530

    fix: blink-cmp config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,26 +1,19 @@
 return {
   'saghen/blink.cmp',
-  lazy = false,
   dependencies = 'rafamadriz/friendly-snippets',
+
   version = 'v0.*',
   opts = {
     keymap = { preset = 'default' },
-    highlight = {
+
+    appearance = {
       use_nvim_cmp_as_default = true,
+      nerd_font_variant = 'mono',
     },
-    nerd_font_variant = 'mono',
+
     sources = {
-      completion = {
-        enabled_providers = { 'lsp', 'path', 'snippets', 'buffer' },
-      },
-    },
-    completion = {
-      menu = {
-        draw = {
-          columns = { { 'label', 'label_description', gap = 1 }, { 'kind_icon', 'kind' } },
-        },
-      },
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
   },
-  opts_extend = { 'sources.completion.enabled_providers' },
+  opts_extend = { 'sources.default' },
 }
