34141ff3a56bdac6fcfd6d95e3109badd9aa507d
Rahul Garg <rg@vihu.dev>
2025-02-06 14:56:58 +0530

    add: nnp

---
+++ lua/plugins/nnp.lua
@@ -0,0 +1,12 @@
+return {
+  'shortcuts/no-neck-pain.nvim',
+  version = '*',
+  keys = {
+    { '<leader>nn', '<cmd>NoNeckPain<cr>', desc = '[N]o [N]eckpain', silent = true },
+    { '<leader>nu', '<cmd>NoNeckPainWidthUp<cr>', desc = '[N]eckpain [U]p', silent = true },
+    { '<leader>nd', '<cmd>NoNeckPainWidthDown<cr>', desc = '[N]eckpain [D]down', silent = true },
+  },
+  opts = {
+    width = 124,
+  },
+}
