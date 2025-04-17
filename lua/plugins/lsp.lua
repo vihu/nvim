93c972f83573660c7bee0b3cc40a358716d02a3a
Rahul Garg <rg@vihu.dev>
2025-04-16 19:42:01 -0600

    update: use prettier, not prettierd

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -168,7 +168,7 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua',
-      'prettierd',
+      'prettier',
       'shfmt',
       'sql-formatter',
       'zls',
