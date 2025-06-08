c64fd43adf510bbe03732c52e4a0d613760e465b
Rahul Garg <rg@vihu.dev>
2025-06-08 17:29:32 -0600

    update: remove snacks

---
+++ lua/plugins/smear.lua
@@ -0,0 +1,10 @@
+return {
+  'sphamba/smear-cursor.nvim',
+  opts = {
+    stiffness = 0.8,
+    trailing_stiffness = 0.7,
+    stiffness_insert_mode = 0.6,
+    trailing_stiffness_insert_mode = 0.6,
+    distance_stop_animating = 0.5,
+  },
+}
